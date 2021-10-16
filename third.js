7e0a2010e2c04362cff67e49733742a7481e5e85
chiyang <chiyanglin@gmail.com>
2021-10-16 15:26:20 +0800

    node js basic tutorial

--- third.js
+++ third.js
@@ -1,9 +1,9 @@
 var http = require('http');
-console.log("this server is runnning on http:localhost:8080 ");
-console.log("Please input : http://localhost:8080/testing ");
+console.log("this server is runnning on http:localhost:4000 ");
+console.log("Please input : http://localhost:4000/testing ");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   //Return the url part of the request object:
   res.write(req.url);
   res.end();
-}).listen(8080);
+}).listen(4000);
